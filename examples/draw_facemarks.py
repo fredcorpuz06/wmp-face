eef4bcc6b89a87922d6bd0c61cbfd88eafaba681
FREDERICK CORPUZ <fcorpuz@wesleyan.edu>
2020-02-21 01:03:18 -0500

    Build out functions to recognize faces in vids

--- examples/draw_facemarks.py
+++ examples/draw_facemarks.py
@@ -1,4 +1,4 @@
-'''
+"""
 ================
 Draw facemarks
 ================
@@ -6,31 +6,33 @@ Draw facemarks
 In this example, we will demonstrate how wmp-face finds faces in input 
 images, crops out a face thumbnail and draws "facemarks"
 
-'''
+"""
 
 # Author: Frederick Corpuz <fcorpuz@wesleyan.edu>
 
 from wmp import detect, utils
 
 
-
 def main():
-    IMG = "../datasets/sample_image.jpg"
-    OUT_DIR = "../datasets/sample-facemarks/"
-    img_array = utils.load_image(IMG)
-    
-    fd = detect.FaceDetector() # can be different detectors
-    unknown_faces = fd.find_faces(img_array)
+    IMG = "wmp/datasets/sample_image.jpg"
+    OUT_DIR = "wmp/datasets/sample-facemarks/"
 
-    cropped_faces = [f.image_data for f in unknown_faces]
+    fd = detect.FaceDetector()  # can be different detectors
+
+    face_image = detect.FaceImage(IMG)
+    unknown_faces = fd.find_faces(face_image)
+
+    cropped_faces = [f.thumbnail_image for f in unknown_faces]
     marked_faces = [f.draw_landmarks() for f in unknown_faces]
 
     utils.write_images(
-        cropped_faces, folder=OUT_DIR, basename="face_thumbnails", 
-        filetype="jpg")
+        cropped_faces, folder=OUT_DIR, basename="face_thumbnails", filetype="jpg"
+    )
 
     utils.write_images(
-        cropped_faces, folder=OUT_DIR, basename="facemarks", filetype="jpg")
+        cropped_faces, folder=OUT_DIR, basename="facemarks", filetype="jpg"
+    )
+
 
 if __name__ == "__main__":
     main()
--- examples/recognize_faces_video.py
+++ examples/recognize_faces_video.py
@@ -1,10 +1,10 @@
 """ 
 ==========================
-Recognize faces in 1 image
+Recognize faces in 1 video
 ==========================
 
 In this example, we will demonstrate how wmp-face finds faces in input
-images, crop out a face thumbnail and predicts a name
+video, crop out a face thumbnail and predicts a name
 """
 
 # Author: Frederick Corpuz <fcorpuz@wesleyan.edu>
@@ -15,17 +15,16 @@ from wmp import detect, utils
 def main():
     FACE_COMP_THRES = 0.6
     ENCODED_REF_DIR = "wmp/datasets/reference-encoded"
-    NEW_IMG = "wmp/datasets/sample_image.jpg"
+    NEW_IMG = "wmp/datasets/sample_video.wmv"
 
-    fd = detect.FaceDetector()
-    fr = detect.FaceRecognizer(ENCODED_REF_DIR)
+    fr = detect.FaceRecognizer()
+    fd = detect.FaceDetector(ENCODED_REF_DIR)
 
-    face_image = detect.FaceImage(NEW_IMG)
-    face_image = fd.find_faces(face_image)  # FaceImage
+    img_array = utils.load_image(NEW_IMG)
+    unknown_faces = fd.find_faces(img_array)
 
-    face_image = fr.predict_names(face_image)
-    print(face_image.retrieve_names())
-
+    predicted_names = [fr.predict_name(f) for f in unknown_faces]
+    print(predicted_names)
     # TODO: return comparison matrix
     # TODO: write face thumbnails w/ predicted names
 
