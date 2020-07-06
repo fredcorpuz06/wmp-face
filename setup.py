c607d81d22c5f57f074dae957d5867cbf21c38c5
FREDERICK CORPUZ <fcorpuz@wesleyan.edu>
2020-07-06 10:34:06 -0400

    Upload minimal requirements

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wmp",  # Replace with your own username
-    version="0.0.1",
+    name="wmp",
+    version="0.1.0",
     author="Frederick Corpuz",
     author_email="fcorpuz@wesleyan.edu",
-    description="A small example package",
+    description="Module for face detection & recognition",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://wmp-face.readthedocs.io",
     packages=setuptools.find_packages(),
+    package_data={"wmp": ["datasets/*.jpg", "datasets/*.mp4"]},
+    install_requires=["dlib>=19.20.0", "numpy>=1.19.0", "Pillow>=7.2.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
